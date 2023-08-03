dce03b254a29e3a719f414e99b5ac42ddd1743d7
사용자 이름 < >
2023-08-04 01:07:10 +0900

    first commit

---
+++ src/main/java/LikeLion11th/YouQuiz_Project/YouQuizProjectApplication.java
@@ -0,0 +1,13 @@
+package LikeLion11th.YouQuiz_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouQuizProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouQuizProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LikeLion11th/YouQuiz_Project/YouQuizProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package LikeLion11th.YouQuiz_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouQuizProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
